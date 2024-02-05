f42d34772c3c0c54d8a82474f8c3bc033effb4a6
Coutin-05 <120342150+Coutin-05@users.noreply.github.com>
2024-02-05 19:09:49 -0300

    Edited calculator function Changed the the file so would pass the npm test. Removed the .skip from the npm test so would check all tests.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return (a + b)
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return (a - b)
 };
 
-const sum = function() {
-	
+const sum = function([...args]) {
+	let total = 0
+    for (const sumArgs of args) {
+        total += sumArgs
+    }
+    return total
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  return array.reduce((product, current) => product * current)
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b)
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+  if (n === 0) return 1;
+  let product = 1;
+  for (let i = n; i > 0; i--) {
+    product *= i;
+  }
+  return product;
 };
 
 // Do not edit below this line
