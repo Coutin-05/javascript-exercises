4d680d5758b710607f1be730dbf34c5c201dde42
Coutin-05 <120342150+Coutin-05@users.noreply.github.com>
2024-02-05 19:22:47 -0300

    Edited palindromes function Changed the the file so would pass the npm test. Removed the .skip from the npm test so would check all tests.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    //regular expression (regex) to remove all non-alphanumeric caracters -punctuation, spaces, symbols- 
+    let re = /[^A-Za-z0-9]/g;
+    //turn everything lowercase
+    lowRegStr = str.toLowerCase().replace(re, "");
+    reverseStr = lowRegStr.split("").reverse().join("");
+    return reverseStr === lowRegStr;
+    
 };
 
 // Do not edit below this line
